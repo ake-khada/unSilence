3fabb08ccdcba385242daf4f1ac282aac442f1e5
Ake <akekhada@tuta.io>
2026-03-08 21:01:25 +0300

    Sprint 4: key management, onboarding screen (create/import/Amber)

--- app/src/main/kotlin/com/unsilence/app/MainActivity.kt
+++ app/src/main/kotlin/com/unsilence/app/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
-import com.unsilence.app.ui.navigation.AppNavigation
+import com.unsilence.app.ui.onboarding.RootScreen
 import com.unsilence.app.ui.theme.UnsilenceTheme
 
 @AndroidEntryPoint
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnsilenceTheme {
-                AppNavigation()
+                RootScreen()
             }
         }
     }
---
+++ app/src/main/kotlin/com/unsilence/app/ui/onboarding/RootViewModel.kt
@@ -0,0 +1,22 @@
+package com.unsilence.app.ui.onboarding
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.unsilence.app.data.auth.KeyManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RootViewModel @Inject constructor(
+    val keyManager: KeyManager,
+) : ViewModel() {
+
+    var isLoggedIn by mutableStateOf(keyManager.hasKey())
+        private set
+
+    fun onOnboardingComplete() {
+        isLoggedIn = true
+    }
+}
